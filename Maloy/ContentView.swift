bf2bf5624a75cfc6e23c2cee7c5fb81aab2766f8
Metabrand <info@metabrand.in>
2025-10-19 18:02:30 +0300

    Initial Commit

---
+++ Maloy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Maloy
+//
+//  Created by dmitry.komissarov on 19/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Maloy/MaloyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaloyApp.swift
+//  Maloy
+//
+//  Created by dmitry.komissarov on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MaloyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
